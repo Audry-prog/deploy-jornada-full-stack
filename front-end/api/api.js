1029800b63d9f187384257bc8fb7bcd48e8e441b
Audry Ávalos <audryavalos@hotmail.com>
2025-02-17 13:12:57 -0300

    Fixed const URL in Front-end api.js file

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 //const { NODE_ENV } = process.env;
-const URL =  "https://deploy-jornada-full-stack-j170.onrender.com//api";
+const URL =  "https://deploy-jornada-full-stack-j170.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
